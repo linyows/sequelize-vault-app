80d9daa86ce9a598fe077db218bd2018cedb2b31
linyows <linyows@gmail.com>
2018-06-28 22:53:37 +0900

    use dotenv

--- models/index.js
+++ models/index.js
@@ -1,27 +1,47 @@
 'use struct';
 
+require('dotenv').config()
 const Sequelize = require('sequelize');
 const SequelizeVault = require('sequelize-vault');
-const fs = require('fs');
+
+let conf = {};
 
 // sequelize-vault
 SequelizeVault.Vault.app = 'express';
-const token = fs.readFileSync('./vault-token', 'utf-8').trim();
-if (token !== '') {
+
+if (process.env.DB_USER && process.env.DB_PASS && process.env.VAULT_TOKEN) {
   SequelizeVault.Vault.enabled = true;
-  SequelizeVault.Vault.token = token;
-  SequelizeVault.Vault.address = process.env.VAULT_ADDR;
+  SequelizeVault.Vault.convergented = true;
+  SequelizeVault.Vault.token = process.env.VAULT_TOKEN;
+  SequelizeVault.Vault.address = 'https://vault.service.consul:8200';
+  conf = {
+    database: 'express',
+    dialect: 'mysql',
+    host: 'mysql.node.consul',
+    port: 3306,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    operatorsAliases: false,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  }
+} else {
+  conf = {
+    database: 'test',
+    dialect: 'sqlite',
+    username: 'root',
+    password: '',
+    storage: ':memory:',
+    operatorsAliases: false,
+  }
 }
 
 // sequelize
-const sequelize = new Sequelize({
-  database: 'test',
-  dialect: 'sqlite',
-  username: 'root',
-  password: '',
-  storage: ':memory:',
-  operatorsAliases: false,
-});
+const sequelize = new Sequelize(conf);
 
 const schema = {
   id: {
